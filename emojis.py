e37431c31581593b8b6b9beeba247f6a8dfb576f
tehki <616658+tehki@users.noreply.github.com>
2023-07-07 07:10:44 +0300

    .

--- emojis.py
+++ emojis.py
@@ -1,13 +1,9 @@
-phone_doge_sticker = "CAACAgIAAx0CZrcwAQACA91km7v5p7WWzuXXhlKornm4OhkuJwACYAkAAhhC7ggkkrp5Mwb6By8E"
-pepe_gun_sticker = "CAACAgIAAx0CZrcwAQACAuBkmzXxgNVyrVMSbb8hO96s_aXQDAAClwsAAk9tKgABwV8Y3W7uTqcvBA"
-pepe_cry_sticker = "CAACAgIAAx0CZrcwAQACAuNkmzX1cCnfipm3a7gliq7IbOjt-QACmQsAAk9tKgABqdaP6kIWIDgvBA"
-pepe_shoot_sticker = "CAACAgIAAx0CZrcwAQACAuZkmzX4k76rSOtq54tDhjOuBhIBMAACnAsAAk9tKgABdWoKS5SWJ5ovBA"
-
-money_bag_emoji = '💰'
-dice_emoji = '🎲'
-darts_emoji = '🎯'
-casino_emoji = '🎰'
-gun_emoji = '🔫'
-phone_emoji = '📱'
+money_bag = '💰'
+dice = '🎲'
+darts = '🎯'
+casino = '🎰'
+gun = '🔫'
+phone = '📱'
 run = '🏃'
 window = '🗔'
+web = '🕸️'
